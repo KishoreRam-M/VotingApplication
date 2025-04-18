59b12e1275a9d8a3314be2e60649af8be16050dc
KishoreRam-M <mrkisho65@outlook.com>
2025-04-18 14:32:29 +0530

    feat:create listing method

--- src/main/java/com/krm/VotingApplication/Controller/PollController.java
+++ src/main/java/com/krm/VotingApplication/Controller/PollController.java
@@ -5,6 +5,7 @@ import com.krm.VotingApplication.Repo.PollRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,17 @@ public class PollController {
             return ResponseEntity.ok(repo.findAll());
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAllPollById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(repo.findById(id));
+
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+
         }
     }
 }
